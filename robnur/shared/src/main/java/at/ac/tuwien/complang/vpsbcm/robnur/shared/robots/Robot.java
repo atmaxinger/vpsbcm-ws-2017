608bae27985bc9ff5fcb527a7cb19abd6f2570c8
Tobias Kain <e1329088@student.tuwien.ac.at>
2017-11-25 00:39:15 +0100

    added log4j

--- robnur/shared/src/main/java/at/ac/tuwien/complang/vpsbcm/robnur/shared/robots/Robot.java
+++ robnur/shared/src/main/java/at/ac/tuwien/complang/vpsbcm/robnur/shared/robots/Robot.java
@@ -1,8 +1,12 @@
 package at.ac.tuwien.complang.vpsbcm.robnur.shared.robots;
 
+import org.apache.log4j.Logger;
+
 public abstract class Robot {
 
     private int id;
+
+    final static Logger logger = Logger.getLogger(Robot.class);
 
     public int getId() {
         return id;
