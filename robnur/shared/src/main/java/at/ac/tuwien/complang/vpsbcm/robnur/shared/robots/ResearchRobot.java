7e53a92ec263d8d397e8510ef15af93c44e411d6
Tobias Kain <e1329088@student.tuwien.ac.at>
2017-12-01 13:09:51 +0100

    fixed research robot

--- robnur/shared/src/main/java/at/ac/tuwien/complang/vpsbcm/robnur/shared/robots/ResearchRobot.java
+++ robnur/shared/src/main/java/at/ac/tuwien/complang/vpsbcm/robnur/shared/robots/ResearchRobot.java
@@ -18,57 +18,82 @@ public class ResearchRobot extends Robot {
         this.compostService = compostService;
         this.configService = configService;
         this.transactionService = transactionService;
+
+        tryUpgradeFlowerPlant();
+        tryUpgradeVegetablePlant();
     }
 
-    public void tryUpgradePlant(){
+    public void tryUpgradeFlowerPlant() {
         Transaction transaction = transactionService.beginTransaction(-1);
 
         List<Flower> flowers = researchService.readAllFlowers(transaction);
-        List<Vegetable> vegetables = researchService.readAllVegetables(transaction);
 
         // try to find a flower-plant to upgrade
-        for (FlowerType flowerType:FlowerType.values()) {
-            List<Flower> flowersOfSameType = getFlowersOfSameType(flowerType,flowers);
+        for (FlowerType flowerType : FlowerType.values()) {
+            List<Flower> upgradableFlowersOfSameType = getUpgradableFlowersOfSameType(flowerType, flowers, 12);
 
-            if(flowersOfSameType.size() >= 12){
-                upgradeFlowerPlant(flowerType,transaction);
-
-                for(int i = 0; i < 12; i++)
-                {
-                    if(getMaxFlowerPlantUpgradeLevel(flowers) < 5) {
-                        researchService.deleteFlower(flowersOfSameType.get(i),transaction);
-                        compostService.putFlower(flowersOfSameType.get(i));
-                    }
-                }
-
-                transaction.commit();
-                tryUpgradePlant();  // try to upgrade another plant
+            // check if there are enough upgradable flowers of the same type
+            if (upgradableFlowersOfSameType == null) {
                 return;
             }
-        }
 
-        // try to find a vegetable-plant to upgrade
-        for (VegetableType vegetableType:VegetableType.values()) {
-            List<Vegetable> vegetablesOfSameType = getVegetablesOfSameType(vegetableType,vegetables);
-            if(vegetablesOfSameType.size() >= 12){
-                upgradeVegetablePlant(vegetableType,transaction);
-
-                for(int i = 0; i < 12; i++)
-                {
-                    if(getMaxVegetablePlantUpgradeLevel(vegetables) < 5) {
-                        researchService.deleteVegetable(vegetables.get(i),transaction);
-                        compostService.putVegetable(vegetablesOfSameType.get(i));
-                    }
-                }
-
-                transaction.commit();
-                tryUpgradePlant();  // try to upgrade another plant
-                return;
+            // remove the flowers from the research-queue
+            for (Flower f : upgradableFlowersOfSameType) {
+                researchService.deleteFlower(f, transaction);
             }
+
+            transaction.commit();
+
+            upgradeFlowerPlant(flowerType);
+
+            // put the flowers on the compost
+            for (Flower f : upgradableFlowersOfSameType) {
+                compostService.putFlower(f);
+            }
+
+            tryUpgradeFlowerPlant();  // try to upgrade another flower plant
+            return;
         }
     }
 
-    private void upgradeFlowerPlant(FlowerType flowerType, Transaction transaction) {
+    public void tryUpgradeVegetablePlant() {
+        Transaction transaction = transactionService.beginTransaction(-1);
+
+        List<Vegetable> vegetables = researchService.readAllVegetables(transaction);
+
+        // try to find a vegetable-plant to upgrade
+        for (VegetableType vegetableType:VegetableType.values()) {
+            List<Vegetable> upgradableVegetablesOfSameType = getUpgradableVegetablesOfSameType(vegetableType,vegetables,12);
+
+            // check if there are enough upgradable vegetables of the same type
+            if(upgradableVegetablesOfSameType == null){
+                return;
+            }
+
+            // remove the vegetables from the research-queue
+            for(Vegetable v : upgradableVegetablesOfSameType)
+            {
+                researchService.deleteVegetable(v,transaction);
+            }
+
+            transaction.commit();
+
+            upgradeVegetablePlant(vegetableType);
+
+            // put the flowers on the compost
+            for(Vegetable v : upgradableVegetablesOfSameType)
+            {
+                compostService.putVegetable(v);
+            }
+
+            tryUpgradeVegetablePlant();  // try to upgrade another plant
+            return;
+        }
+    }
+
+    private void upgradeFlowerPlant(FlowerType flowerType) {
+        Transaction transaction = transactionService.beginTransaction(-1);
+
         List<FlowerPlantCultivationInformation> flowerPlantCultivationInformation = configService.readAllFlowerPlantCultivationInformation(transaction);
         FlowerPlantCultivationInformation cultivationInformationOfFlowerType = null;
 
@@ -83,9 +108,14 @@ public class ResearchRobot extends Robot {
 
         configService.deleteFlowerPlantCultivationInformation(cultivationInformationOfFlowerType.getId(),transaction);
         configService.putFlowerPlantCultivationInformation(cultivationInformationOfFlowerType,transaction);
+
+        waitResearchTime();
+        transaction.commit();
     }
 
-    private void upgradeVegetablePlant(VegetableType vegetableType, Transaction transaction){
+    private void upgradeVegetablePlant(VegetableType vegetableType){
+        Transaction transaction = transactionService.beginTransaction(-1);
+
         List<VegetablePlantCultivationInformation> vegetablePlantCultivationInformation = configService.readAllVegetablePlantCultivationInformation(transaction);
         VegetablePlantCultivationInformation cultivationInformationOfVegetableType = null;
 
@@ -100,6 +130,9 @@ public class ResearchRobot extends Robot {
 
         configService.deleteVegetablePlantCultivationInformation(cultivationInformationOfVegetableType.getId(),transaction);
         configService.putVegetablePlantCultivationInformation(cultivationInformationOfVegetableType,transaction);
+
+        waitResearchTime();
+        transaction.commit();
     }
 
     private CultivationInformation upgradeCultivationInformation(CultivationInformation cultivationInformation){
@@ -127,59 +160,62 @@ public class ResearchRobot extends Robot {
         return cultivationInformation;
     }
 
-    List<Flower> getFlowersOfSameType(FlowerType flowerType, List<Flower> flowers){
+    List<Flower> getUpgradableFlowersOfSameType(FlowerType flowerType, List<Flower> flowers, int amount){
 
         List<Flower> flowersOfSameType = new ArrayList<>();
 
         for (Flower f:flowers) {
-            if (f.getParentFlowerPlant().getCultivationInformation().getFlowerType().equals(flowerType)){
+            if (f.getParentFlowerPlant().getCultivationInformation().getFlowerType().equals(flowerType) &&
+                    f.getParentFlowerPlant().getCultivationInformation().getUpgradeLevel() < 5){
+
                 flowersOfSameType.add(f);
+
+                amount --;
+                if(amount == 0){
+                    break;
+                }
             }
+        }
+
+        // check if <amount> upgradable flowers were available
+        if(amount != 0){
+            return null;
         }
 
         return flowersOfSameType;
     }
 
-    List<Vegetable> getVegetablesOfSameType(VegetableType vegetableType, List<Vegetable> vegetables){
+    List<Vegetable> getUpgradableVegetablesOfSameType(VegetableType vegetableType, List<Vegetable> vegetables, int amount){
 
         List<Vegetable> vegetablesOfSameType = new ArrayList<>();
 
         for (Vegetable v:vegetables) {
-            if (v.getParentVegetablePlant().getCultivationInformation().getVegetableType().equals(vegetableType)){
+            if (v.getParentVegetablePlant().getCultivationInformation().getVegetableType().equals(vegetableType) &&
+                    v.getParentVegetablePlant().getCultivationInformation().getUpgradeLevel() < 5){
+
                 vegetablesOfSameType.add(v);
+
+                amount --;
+                if(amount == 0){
+                    break;
+                }
             }
         }
+
+        // check if <amount> upgradable flowers were available
+        if(amount != 0){
+            return null;
+        }
+
 
         return vegetablesOfSameType;
     }
 
-    private int getMaxFlowerPlantUpgradeLevel(List<Flower> flowers){
-
-        int maxUpgradeLevel = 0;
-
-        for (Flower f:flowers) {
-            int upgradeLevel = f.getParentFlowerPlant().getCultivationInformation().getUpgradeLevel();
-
-            if(upgradeLevel > maxUpgradeLevel){
-                maxUpgradeLevel = upgradeLevel;
-            }
+    private void waitResearchTime(){
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
         }
-
-        return maxUpgradeLevel;
-    }
-
-    private int getMaxVegetablePlantUpgradeLevel(List<Vegetable> vegetables){
-
-        int maxUpgradeLevel = 0;
-
-        for (Vegetable v:vegetables) {
-            int upgradeLevel = v.getParentVegetablePlant().getCultivationInformation().getUpgradeLevel();
-
-            if(upgradeLevel > maxUpgradeLevel){
-                maxUpgradeLevel = upgradeLevel;
-            }
-        }
-
-        return maxUpgradeLevel;
     }
 }
