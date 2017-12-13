6a670a2071bf04f7ef0e6fcc4e35f1d8ce35b0b6
Tobias Kain <e1329088@student.tuwien.ac.at>
2017-12-13 10:22:03 +0100

    documented packing service

--- robnur/shared/src/main/java/at/ac/tuwien/complang/vpsbcm/robnur/shared/services/PackingService.java
+++ robnur/shared/src/main/java/at/ac/tuwien/complang/vpsbcm/robnur/shared/services/PackingService.java
@@ -29,15 +29,47 @@ public abstract class PackingService {
         }
     }
 
+    /**
+     * puts a flower into the packing department
+     * @param flower
+     * @param transaction
+     */
     public abstract void putFlower(Flower flower, Transaction transaction);
 
+    /**
+     * puts a vegetable into the packing department
+     * @param vegetable
+     * @param transaction
+     */
     public abstract void putVegetable(Vegetable vegetable,Transaction transaction);
 
+    /**
+     * takes a specific flower form the packing department
+     * @param flowerId id of the flower
+     * @param transaction
+     * @return the flower with the specified id, null if flower does not exist
+     */
     public abstract Flower getFlower(String flowerId, Transaction transaction);
 
+    /**
+     * takes a specific vegetable form the packing department
+     * @param vegetableId id of the vegetable
+     * @param transaction
+     * @return the vegetable with the specified id, null if vegetable does not exist
+     */
     public abstract Vegetable getVegetable(String vegetableId, Transaction transaction);
 
+    /**
+     * reads all flowers that are in the packing department
+     * @param transaction
+     * @return all flowers that are in the packing department, empty list if there are no flowers in the packing department
+     */
     public abstract List<Flower> readAllFlowers(Transaction transaction);
 
+    /**
+     * reads all vegetables that are in the packing department
+     * @param transaction
+     * @return all vegetables that are in the packing department, empty list if there are no vegetables in the packing department
+     */
     public abstract List<Vegetable> readAllVegetables(Transaction transaction);
 }
