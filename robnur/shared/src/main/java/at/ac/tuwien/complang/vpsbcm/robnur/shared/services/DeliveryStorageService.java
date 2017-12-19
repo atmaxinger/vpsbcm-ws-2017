6df17d3379cf6ff20f28eb25482967c450e0d102
atmaxinger <maxi6594@gmail.com>
2017-12-19 20:09:39 +0100

    log delivered baskets and bouqets

--- robnur/shared/src/main/java/at/ac/tuwien/complang/vpsbcm/robnur/shared/services/DeliveryStorageService.java
+++ robnur/shared/src/main/java/at/ac/tuwien/complang/vpsbcm/robnur/shared/services/DeliveryStorageService.java
@@ -6,17 +6,31 @@ import at.ac.tuwien.complang.vpsbcm.robnur.shared.plants.VegetableBasket;
 import java.util.List;
 
 public abstract class DeliveryStorageService {
+    private org.apache.log4j.Logger logger = org.apache.log4j.Logger.getLogger(DeliveryStorageService.class);
+
 
     private StorageService.Callback<List<VegetableBasket>> vegetableBasketsChanged;
     private StorageService.Callback<List<Bouquet>> bouqetsChanged;
 
     protected void notifyVegetableBasketsChanged() {
+        logger.debug(String.format("vegetable baskets changed"));
+        List<VegetableBasket> baskets = readAllVegetableBaskets();
+        for(VegetableBasket basket : baskets) {
+            logger.debug(String.format("  %s", basket.getId()));
+        }
+
         if(vegetableBasketsChanged != null) {
-            vegetableBasketsChanged.handle(readAllVegetableBaskets());
+            vegetableBasketsChanged.handle(baskets);
         }
     }
 
     protected void notifyBouqetsChanged() {
+        logger.debug(String.format("bouqets changed"));
+        List<Bouquet> bouquets = readAllBouqets();
+        for(Bouquet bouquet : bouquets) {
+            logger.debug(String.format("  %s", bouquet.getId()));
+        }
+
         if(bouqetsChanged != null) {
             bouqetsChanged.handle(readAllBouqets());
         }
