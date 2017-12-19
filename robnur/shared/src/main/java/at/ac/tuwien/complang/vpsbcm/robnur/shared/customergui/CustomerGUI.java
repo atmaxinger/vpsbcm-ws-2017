e2d7d607f0d7dd76c18a5b5f7287010dadd519c1
atmaxinger <maxi6594@gmail.com>
2017-12-19 20:21:57 +0100

    customer gui: be able to restart gui

--- robnur/shared/src/main/java/at/ac/tuwien/complang/vpsbcm/robnur/shared/customergui/CustomerGUI.java
+++ robnur/shared/src/main/java/at/ac/tuwien/complang/vpsbcm/robnur/shared/customergui/CustomerGUI.java
@@ -7,12 +7,15 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.control.Accordion;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 import org.apache.log4j.Logger;
 
 import java.io.IOException;
+import java.util.Optional;
 
 public class CustomerGUI extends Application {
     final static Logger logger = Logger.getLogger(CustomerGUI.class);
@@ -22,7 +25,10 @@ public class CustomerGUI extends Application {
     public static String address;
     public static DeliveryStorageService deliveryStorageService;
 
+    private String[] args;
+
     public void execute(String[] args) {
+        this.args = args;
         launch(args);
     }
 
@@ -37,8 +43,33 @@ public class CustomerGUI extends Application {
         }
         Scene scene = new Scene(root);
 
+        primaryStage.setOnCloseRequest(value -> {
+            runDialog();
+        });
+
         primaryStage.setTitle("Blumenshop");
         primaryStage.setScene(scene);
         primaryStage.show();
+    }
+
+
+    private void runDialog() {
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+        alert.setTitle("Beenden");
+        alert.setHeaderText("Soll das Programm beendet werden oder nur die GUI neu gestartet werden?");
+        alert.setContentText("Choose your option.");
+
+        ButtonType buttonTypeQuit = new ButtonType("Programm beenden");
+        ButtonType buttonTypeRestart = new ButtonType("GUI neu starten");
+
+        alert.getButtonTypes().setAll(buttonTypeQuit, buttonTypeRestart);
+
+        Optional<ButtonType> result = alert.showAndWait();
+        if (result.get() == buttonTypeRestart){
+            Stage stage = new Stage();
+            start(stage);
+        } else if (result.get() == buttonTypeQuit) {
+            System.exit(0);
+        }
     }
 }
