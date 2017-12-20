3950a539164e120c40e3ef7f33fb0fe3a3f2ebd7
atmaxinger <maxi6594@gmail.com>
2017-12-20 10:59:52 +0100

    customer gui: disable orders if can not place order

--- robnur/shared/src/main/java/at/ac/tuwien/complang/vpsbcm/robnur/shared/customergui/OrdersController.java
+++ robnur/shared/src/main/java/at/ac/tuwien/complang/vpsbcm/robnur/shared/customergui/OrdersController.java
@@ -4,6 +4,7 @@ import at.ac.tuwien.complang.vpsbcm.robnur.shared.plants.FlowerPlantCultivationI
 import at.ac.tuwien.complang.vpsbcm.robnur.shared.plants.VegetablePlantCultivationInformation;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.layout.HBox;
 
 import java.util.LinkedList;
 import java.util.List;
@@ -11,13 +12,24 @@ import java.util.List;
 public class OrdersController {
     public Button btnOrderFlowers;
     public Button btnOrderVegetables;
+    public HBox box;
 
 
     List<FlowerPlantCultivationInformation> fpcis = new LinkedList<>();
     List<VegetablePlantCultivationInformation> vpcis = new LinkedList<>();
 
+
+    private void updateCanPlaceOrder(boolean canPlaceOrder) {
+        box.setDisable(!canPlaceOrder);
+    }
+
     @FXML
     public void initialize() {
+        updateCanPlaceOrder(CustomerGUI.orderService.canPlaceOrder(CustomerGUI.address));
+        CustomerGUI.orderService.onCanPlaceOrderChanged(p -> {
+            updateCanPlaceOrder(CustomerGUI.orderService.canPlaceOrder(CustomerGUI.address));
+        });
+
         vpcis = CustomerGUI.configService.readAllVegetablePlantCultivationInformation(null);
         fpcis = CustomerGUI.configService.readAllFlowerPlantCultivationInformation(null);
 
